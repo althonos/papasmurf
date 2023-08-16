c2d463045cc0ebb89eff7a9be9a2506d1aae7603
Martin Larralde <martin.larralde@embl.de>
2023-08-16 22:31:37 +0200

    Use arena allocation and `u32` indices to reduce node size in `KmerTrie`

--- src/db/kmertrie.rs
+++ src/db/kmertrie.rs
@@ -1,50 +1,59 @@
+use std::num::NonZeroU32;
+
 #[derive(Default, Debug, Clone)]
 struct TrieNode {
-    a: Option<Box<TrieNode>>,
-    c: Option<Box<TrieNode>>,
-    g: Option<Box<TrieNode>>,
-    t: Option<Box<TrieNode>>,
+    a: Option<NonZeroU32>,
+    c: Option<NonZeroU32>,
+    g: Option<NonZeroU32>,
+    t: Option<NonZeroU32>,
 }
 
 #[derive(Default, Debug, Clone)]
 pub struct KmerTrie {
-    root: TrieNode,
+    storage: Vec<TrieNode>,
     k: usize,
-    pub nodes: usize,
 }
 
 impl KmerTrie {
     pub fn new(k: usize) -> Self {
         Self {
             k,
-            root: TrieNode::default(),
-            nodes: 1,
+            storage: vec![TrieNode::default()],
         }
     }
 
     pub fn insert(&mut self, word: &str) {
         assert_eq!(word.len(), self.k);
 
-        let mut current_node = &mut self.root;
+        let mut current_node = 0;
         for c in word.chars() {
-            current_node = match c {
-                'A' => current_node.a.get_or_insert_with(|| {
-                    self.nodes += 1;
-                    Default::default()
-                }),
-                'C' => current_node.c.get_or_insert_with(|| {
-                    self.nodes += 1;
-                    Default::default()
-                }),
-                'G' => current_node.g.get_or_insert_with(|| {
-                    self.nodes += 1;
-                    Default::default()
-                }),
-                'T' => current_node.t.get_or_insert_with(|| {
-                    self.nodes += 1;
-                    Default::default()
-                }),
-                _ => unreachable!(),
+            current_node = {
+                let mut node = std::mem::take(&mut self.storage[current_node as usize]);
+                let x = match c {
+                    'A' => node.a.get_or_insert_with(|| {
+                        let n = NonZeroU32::new( self.storage.len().try_into().unwrap() ).unwrap();
+                        self.storage.push(Default::default());
+                        n
+                    }).get(),
+                    'C' => node.c.get_or_insert_with(|| {
+                        let n = NonZeroU32::new( self.storage.len().try_into().unwrap() ).unwrap();
+                        self.storage.push(Default::default());
+                        n
+                    }).get(),
+                    'G' => node.g.get_or_insert_with(|| {
+                        let n = NonZeroU32::new( self.storage.len().try_into().unwrap() ).unwrap();
+                        self.storage.push(Default::default());
+                        n
+                    }).get(),
+                    'T' => node.t.get_or_insert_with(|| {
+                        let n = NonZeroU32::new( self.storage.len().try_into().unwrap() ).unwrap();
+                        self.storage.push(Default::default());
+                        n
+                    }).get(),
+                    _ => unreachable!(),
+                };
+                self.storage[current_node as usize] = node;
+                x
             };
         }
     }
@@ -52,17 +61,17 @@ impl KmerTrie {
     pub fn contains(&self, word: &str) -> bool {
         assert_eq!(word.len(), self.k);
 
-        let mut current_node = &self.root;
+        let mut current_node = 0;
         for c in word.chars() {
             let option = match c {
-                'A' => &current_node.a,
-                'C' => &current_node.c,
-                'G' => &current_node.g,
-                'T' => &current_node.t,
+                'A' => &self.storage[current_node as usize].a,
+                'C' => &self.storage[current_node as usize].c,
+                'G' => &self.storage[current_node as usize].g,
+                'T' => &self.storage[current_node as usize].t,
                 _ => unreachable!(),
             };
             match option {
-                Some(node) => current_node = node,
+                Some(node) => current_node = node.get(),
                 None => return false,
             }
         }
@@ -75,8 +84,8 @@ impl KmerTrie {
             panic!("{} < {}", word.len(), self.k);
         }
 
-        struct State<'a> {
-            node: &'a TrieNode,
+        struct State {
+            node: u32,
             mismatches: usize,
             position: usize,
             prefix: String,
@@ -86,7 +95,7 @@ impl KmerTrie {
         let mut bytes = word.as_bytes();
         let mut stack = Vec::new();
         stack.push(State {
-            node: &self.root,
+            node: 0,
             mismatches: 0,
             position: 0,
             prefix: String::new(),
@@ -97,40 +106,40 @@ impl KmerTrie {
                 results.push((state.prefix, state.mismatches));
             } else {
                 let c = bytes[state.position];
-                if let Some(node) = &state.node.a {
+                if let Some(node) = &self.storage[state.node as usize].a {
                     if c == b'A' || state.mismatches < max_mismatches {
                         stack.push(State {
-                            node,
+                            node: node.get(),
                             mismatches: state.mismatches + (c != b'A') as usize,
                             position: state.position + 1,
                             prefix: format!("{}A", &state.prefix),
                         })
                     }
                 }
-                if let Some(node) = &state.node.c {
+                if let Some(node) = &self.storage[state.node as usize].c {
                     if c == b'C' || state.mismatches < max_mismatches {
                         stack.push(State {
-                            node,
+                            node: node.get(),
                             mismatches: state.mismatches + (c != b'C') as usize,
                             position: state.position + 1,
                             prefix: format!("{}C", &state.prefix),
                         })
                     }
                 }
-                if let Some(node) = &state.node.g {
+                if let Some(node) = &self.storage[state.node as usize].g {
                     if c == b'G' || state.mismatches < max_mismatches {
                         stack.push(State {
-                            node,
+                            node: node.get(),
                             mismatches: state.mismatches + (c != b'G') as usize,
                             position: state.position + 1,
                             prefix: format!("{}G", &state.prefix),
                         })
                     }
                 }
-                if let Some(node) = &state.node.t {
+                if let Some(node) = &self.storage[state.node as usize].t {
                     if c == b'T' || state.mismatches < max_mismatches {
                         stack.push(State {
-                            node,
+                            node: node.get(),
                             mismatches: state.mismatches + (c != b'T') as usize,
                             position: state.position + 1,
                             prefix: format!("{}T", &state.prefix),
@@ -175,7 +184,9 @@ mod test {
         results.sort();
 
         assert_eq!(results.len(), 2);
-        assert_eq!(results[0], "ATTA");
-        assert_eq!(results[1], "ATTT");
+        assert_eq!(results[0].0, "ATTA");
+        assert_eq!(results[1].0, "ATTT");
+        assert_eq!(results[0].1, 0);
+        assert_eq!(results[1].1, 1);
     }
 }
