5a181f43ade7d65284e3ade245dc8a68b3a21f9a
Martin Larralde <martin.larralde@embl.de>
2026-07-26 18:20:15 +0200

    Implement function to get duplicate columns for a CSC matrix

--- papasmurf/src/matrix/csc.rs
+++ papasmurf/src/matrix/csc.rs
@@ -1,4 +1,6 @@
 use std::iter::FusedIterator;
+use std::println;
+use std::unimplemented;
 
 use serde::Deserialize;
 use serde::Serialize;
@@ -24,6 +26,57 @@ impl<T> CscMatrix<T> {
             row_index: Vec::new(),
             col_index: vec![0; cols + 1],
         }
+    }
+}
+
+impl<T: PartialEq> CscMatrix<T> {
+    /// Return a boolean vector denoting duplicate columns.
+    /// 
+    /// The first column with unique values 
+    pub fn duplicate_columns(&self) -> Vec<bool> {
+        // Compute number of nonzero elements per columns
+        let mut lengths = vec![0; self.columns()];
+        for i in 1..=self.columns() {
+            lengths[i - 1] = self.col_index[i] - self.col_index[i - 1];
+        }
+
+        // Sort columns by number of nonzero elements
+        let mut indices = (0..self.columns()).collect::<Vec<_>>();
+        indices.sort_by_key(|&j| (lengths[j], j));
+
+        // Prepare the result vector
+        let mut dup = vec![false; self.columns()];
+
+        // Iterate by groups
+        let mut i = 0;
+        while i < indices.len() {
+            // Advance until a larger column is reached
+            let mut i2 = i;
+            while i2 < indices.len() && lengths[indices[i2]] == lengths[indices[i]] {
+                i2 += 1;
+            }
+            // Get group of indices with equal length
+            let group = &indices[i..i2];
+            debug_assert!(group.iter().all(|&j| lengths[j] == lengths[group[0]]));
+            i = i2;
+            // Compare columns pair by pair 
+            for (n, &j1) in group.iter().enumerate() {
+                for &j2 in group[n+1..].iter() {
+                    if dup[j2] {
+                        continue;
+                    }
+                    let r1 = &self.row_index[self.col_index[j1]..self.col_index[j1+1]];
+                    let r2 = &self.row_index[self.col_index[j2]..self.col_index[j2+1]];
+                    let d1 = &self.data[self.col_index[j1]..self.col_index[j1+1]];
+                    let d2 = &self.data[self.col_index[j2]..self.col_index[j2+1]];
+                    if r1 == r2 && d1 == d2 {
+                        dup[j2] = true;
+                    }
+                }
+            }
+        }
+
+        dup
     }
 }
 
@@ -121,5 +174,45 @@ mod test {
         assert_eq!(it.next(), Some((1, 0, &3)));
         assert_eq!(it.next(), Some((0, 1, &2)));
         assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn duplicate_columns() {
+        let m1 = {
+            let mut a = DokMatrix::<u8>::new(2, 4);
+            a.insert(0, 1, 1);
+            a.insert(1, 2, 1);
+            a.insert(0, 3, 1);
+            a.insert(1, 3, 1);
+            a.to_csc()
+        };
+        assert_eq!(m1.duplicate_columns(), vec![false, false, false, false]);
+
+        let m2 = {
+            let mut a = DokMatrix::<u8>::new(2, 4);
+            a.insert(0, 1, 1);
+            a.insert(0, 3, 1);
+            a.to_csc()
+        };
+        assert_eq!(m2.duplicate_columns(), vec![false, false, true, true]);
+
+        let m3 = {
+            let mut a = DokMatrix::<u8>::new(2, 4);
+            a.insert(0, 1, 1);
+            a.insert(1, 1, 1);
+            a.insert(1, 2, 1);
+            a.insert(0, 3, 1);
+            a.insert(1, 3, 1);
+            a.to_csc()
+        };
+        assert_eq!(m3.duplicate_columns(), vec![false, false, false, true]);
+
+        let m4 = {
+            let mut a = DokMatrix::<u8>::new(2, 4);
+            a.insert(0, 1, 1);
+            a.insert(0, 3, 2);
+            a.to_csc()
+        };
+        assert_eq!(m4.duplicate_columns(), vec![false, false, true, false]);
     }
 }
