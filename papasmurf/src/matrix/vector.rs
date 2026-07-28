3afbd03fe59a188148d23559957a98b6b92017d7
Martin Larralde <martin.larralde@embl.de>
2026-07-29 00:25:01 +0200

    Implement vector-matrix product with blanket trait implementation

--- papasmurf/src/matrix/vector.rs
+++ papasmurf/src/matrix/vector.rs
@@ -1,5 +1,13 @@
+use std::ops::AddAssign;
 use std::ops::Index;
 use std::ops::IndexMut;
+use std::ops::Mul;
+use std::process::Output;
+
+use crate::matrix::MatrixDimensions;
+use crate::matrix::NonZeroElements;
+
+use super::Dot;
 
 #[derive(Debug, Clone)]
 pub struct Vector<T> {
@@ -48,5 +56,37 @@ impl<T> Index<usize> for Vector<T> {
 impl<T> IndexMut<usize> for Vector<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.data[index]
+    }
+}
+
+impl<T, M> Dot<M> for Vector<T>
+where
+    T: AddAssign + Mul<Output = T> + PartialEq + Clone + Default,
+    M: MatrixDimensions + for<'m> NonZeroElements<'m, Elem = T>,
+{
+    type Output = Vector<T>;
+    fn dot(self, mat: M) -> Self::Output {
+        assert_eq!(self.len(), mat.rows());
+        let mut out = Vector::new(mat.columns());
+        for (i, j, x) in mat.non_zero_elements() {
+            out[j] += self[i].clone() * x.clone();
+        }
+        out
+    }
+}
+
+impl<T, M> Dot<Vector<T>> for M
+where
+    T: AddAssign + Mul<Output = T> + PartialEq + Clone + Default,
+    M: MatrixDimensions + for<'m> NonZeroElements<'m, Elem = T>,
+{
+    type Output = Vector<T>;
+    fn dot(self, v: Vector<T>) -> Self::Output {
+        assert_eq!(self.columns(), v.len());
+        let mut out = Vector::new(self.rows());
+        for (i, j, x) in self.non_zero_elements() {
+            out[i] += x.clone() * v[j].clone();
+        }
+        out
     }
 }
