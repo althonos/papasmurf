79a209d409c2bd81e3e9bd0709c740bd3996df0c
Martin Larralde <martin.larralde@embl.de>
2026-07-29 00:52:35 +0200

    Add more math operations to `Vector`

--- papasmurf/src/matrix/vector.rs
+++ papasmurf/src/matrix/vector.rs
@@ -1,8 +1,9 @@
+use std::iter::Sum;
 use std::ops::AddAssign;
+use std::ops::DivAssign;
 use std::ops::Index;
 use std::ops::IndexMut;
 use std::ops::Mul;
-use std::process::Output;
 
 use crate::matrix::MatrixDimensions;
 use crate::matrix::NonZeroElements;
@@ -18,12 +19,32 @@ impl<T> Vector<T> {
     pub fn len(&self) -> usize {
         self.data.len()
     }
+
+    pub fn iter(&self) -> <&Vec<T> as IntoIterator>::IntoIter {
+        self.data.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> <&mut Vec<T> as IntoIterator>::IntoIter {
+        self.data.iter_mut()
+    }
 }
 
 impl<T: Default> Vector<T> {
     pub fn new(length: usize) -> Self {
         Self {
             data: (0..length).map(|_| T::default()).collect(),
+        }
+    }
+}
+
+impl<T> Vector<T>
+where
+    T: for<'a> Sum<&'a T> + DivAssign + Clone,
+{
+    pub fn normalize(&mut self) {
+        let total = self.data.iter().sum::<T>();
+        for x in self.data.iter_mut() {
+            *x /= total.clone();
         }
     }
 }
@@ -59,13 +80,24 @@ impl<T> IndexMut<usize> for Vector<T> {
     }
 }
 
-impl<T, M> Dot<M> for Vector<T>
+impl<T, M> Dot<&M> for Vector<T>
 where
     T: AddAssign + Mul<Output = T> + PartialEq + Clone + Default,
     M: MatrixDimensions + for<'m> NonZeroElements<'m, Elem = T>,
 {
     type Output = Vector<T>;
-    fn dot(self, mat: M) -> Self::Output {
+    fn dot(self, mat: &M) -> Self::Output {
+        (&self).dot(mat)
+    }
+}
+
+impl<T, M> Dot<&M> for &Vector<T>
+where
+    T: AddAssign + Mul<Output = T> + PartialEq + Clone + Default,
+    M: MatrixDimensions + for<'m> NonZeroElements<'m, Elem = T>,
+{
+    type Output = Vector<T>;
+    fn dot(self, mat: &M) -> Self::Output {
         assert_eq!(self.len(), mat.rows());
         let mut out = Vector::new(mat.columns());
         for (i, j, x) in mat.non_zero_elements() {
@@ -75,18 +107,45 @@ where
     }
 }
 
-impl<T, M> Dot<Vector<T>> for M
+impl<T, M> Dot<Vector<T>> for &M
 where
     T: AddAssign + Mul<Output = T> + PartialEq + Clone + Default,
     M: MatrixDimensions + for<'m> NonZeroElements<'m, Elem = T>,
 {
     type Output = Vector<T>;
     fn dot(self, v: Vector<T>) -> Self::Output {
+        self.dot(&v)
+    }
+}
+
+impl<T, M> Dot<&Vector<T>> for &M
+where
+    T: AddAssign + Mul<Output = T> + PartialEq + Clone + Default,
+    M: MatrixDimensions + for<'m> NonZeroElements<'m, Elem = T>,
+{
+    type Output = Vector<T>;
+    fn dot(self, v: &Vector<T>) -> Self::Output {
         assert_eq!(self.columns(), v.len());
         let mut out = Vector::new(self.rows());
         for (i, j, x) in self.non_zero_elements() {
             out[i] += x.clone() * v[j].clone();
         }
         out
+    }
+}
+
+impl<T> IntoIterator for Vector<T> {
+    type Item = <Vec<T> as IntoIterator>::Item;
+    type IntoIter = <Vec<T> as IntoIterator>::IntoIter;
+    fn into_iter(self) -> Self::IntoIter {
+        self.data.into_iter()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a Vector<T> {
+    type Item = <&'a Vec<T> as IntoIterator>::Item;
+    type IntoIter = <&'a Vec<T> as IntoIterator>::IntoIter;
+    fn into_iter(self) -> Self::IntoIter {
+        self.data.iter()
     }
 }
